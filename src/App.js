66ff998af51c44e15b0a4e993421ad815ea57261
kenmillionaire <osobsim@gmail.com>
2023-05-01 12:17:54 -0400

    Added the Github repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/kehinde-oyeronke-osoba/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kehinde Oyeronke Osoba
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kenmillionaire/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
