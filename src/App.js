04a18a6d92d21e4587ccbc935b2663bba7a9ba3d
kenmillionaire <osobsim@gmail.com>
2023-05-01 17:39:29 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/kehinde-oyeronke-osoba/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kehinde Oyeronke Osoba
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kenmillionaire/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-Sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Home</button>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/kehinde-oyeronke-osoba/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kehinde Oyeronke Osoba
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kenmillionaire/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
