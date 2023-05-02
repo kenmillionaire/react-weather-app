66cf6b7d1a715ce63dab4f41b6792c1494a95ec9
kenmillionaire <osobsim@gmail.com>
2023-05-02 10:26:33 -0400

    Basic Styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        {/* <h1>Weather App</h1>
-        <button className="btn btn-primary">Home</button> */}
         <Weather />
 
         <footer>
